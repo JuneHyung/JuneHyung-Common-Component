c8bb26b5f52d5d10061d89657740203e21fed8fb
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2022-06-07 21:30:57 +0900

    scss test

--- junehyung_component/.eslintrc.js
+++ junehyung_component/.eslintrc.js
@@ -13,8 +13,21 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/global.scss";`
+      }
+    }
+  },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+          endOfLine: 'auto',
+      },
+  ],
   },
 };
