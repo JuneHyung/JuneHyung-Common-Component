cfb29d3db1cdf8e3ed8877153b3a0f6475964a80
JuneHyung <46647782+JuneHyung@users.noreply.github.com>
2022-06-13 23:00:15 +0900

    margin, padding설정

--- junehyung_component/src/views/About.vue
+++ junehyung_component/src/views/About.vue
@@ -3,12 +3,17 @@
     <h1>This is an about page</h1>
   </div>
   <div class="testBox"></div>
+  <div class="testBox2 j-ma-lg j-pa-sm"></div>
 </template>
 <style lang="scss" scoped>
 .testBox {
   width: 200px;
   height: 200px;
   background-color: $bgColor;
-  margin-left: $marginLeft;
+}
+.testBox2 {
+  width: 200px;
+  height: 200px;
+  background-color: $bgColor2;
 }
 </style>
